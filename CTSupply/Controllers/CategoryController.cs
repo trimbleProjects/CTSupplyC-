1f873ec6669916ff5c3b6fdbf352bb74083fe6a4
Corey Trimble <ctrimble@scfastening.com>
2021-07-05 20:13:17 -0400

    Add project files.

---
+++ CTSupply/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using  CTSupply.Models;
+
+namespace CTSupply.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            Product prods = new Product();
+
+            List<Category> productList = prods.products.ToList();
+
+
+
+            return View(productList);
+
+        }
+    }
+}
---
+++ CTSupply/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CTSupply.Models
+{
+    [Table("Products")]
+    public class Category 
+    {
+        public string Partnum { get; set; }
+
+
+    }
+    public class Product : DbContext
+    {
+        public DbSet<Category> products { get; set; }
+    }
+}
